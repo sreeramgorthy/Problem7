79e6867871329036454131cebf2f71061eae4ccc
sreeramgorthy <sreeramgorthy@gmail.com>
2020-12-03 00:08:27 +0530

    Build filter using basic building blocks in Simulink. Cascade realization of system function.

---
+++ projectUtils/shutDownProject.m
@@ -0,0 +1,4 @@
+% This script will execute at the close of the project.
+clc
+clearvars
+bdclose all
---
+++ projectUtils/startUpProject.m
@@ -0,0 +1,4 @@
+% This script will execute at the start of the project.
+clc
+clearvars
+bdclose all
